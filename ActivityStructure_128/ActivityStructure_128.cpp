301f2446b281be8f70a13d6596e3f7a2d329d275
iqwa1107 <ramahs49@gmail.com>
2023-04-03 10:24:57 +0700

    display mhs 1

--- ActivityStructure_128/ActivityStructure_128.cpp
+++ ActivityStructure_128/ActivityStructure_128.cpp
@@ -26,6 +26,10 @@ int main() {
 	cout << "Masukan Umur : ";
 	cin >> mhs2.umur;
 
-	cout << "/nNim = " << mhs1.NIM
-    
+	cout << "/nNim = " << mhs1.NIM;
+	cout << "/nNama = " << mhs1.nama;
+	cout << "/nalamat = " << mhs1.alamat;
+	cout << "/numur = " << mhs1.umur;
+
+
 }
