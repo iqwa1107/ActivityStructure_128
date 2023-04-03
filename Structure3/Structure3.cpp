9751a55a5f5614ab580caef80d04ce2ca4e0db1f
iqwa1107 <ramahs49@gmail.com>
2023-04-03 11:24:04 +0700

    display

--- Structure3/Structure3.cpp
+++ Structure3/Structure3.cpp
@@ -34,4 +34,13 @@ int main() {
 			cin.ignore(1, '\n');
 
 		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			cout << "\n\nNIM = " << mhs[i].NIM;
+			cout << "\nNama = " << mhs[i].nama;
+			cout << "\nDesa = " << mhs[i].alamat.desa;
+			cout << "\nKota = " << mhs[i].alamat.kota;
+			cout << "\nUmur = " << mhs[i].umur;
+		}
 }
