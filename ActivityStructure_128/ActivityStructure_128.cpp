d4460481c976a9d75abcc65df5f83f5742695c7d
iqwa1107 <ramahs49@gmail.com>
2023-04-03 10:22:45 +0700

    int main

--- ActivityStructure_128/ActivityStructure_128.cpp
+++ ActivityStructure_128/ActivityStructure_128.cpp
@@ -8,3 +8,24 @@ struct mahasiswa
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "2020220100";
+	mhs1.nama = "ardian mulya";
+	mhs1.alamat = "wonosbo";
+	mhs1.umur = 28;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukan alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+
+	cout << "/nNim = " << mhs1.NIM
+    
+}
